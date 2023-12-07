53b2c67198af3de0896ea4e03fc8a3f797f1a53b
michael <michael1241@users.noreply.github.com>
2023-12-07 12:08:09 +0000

    remove repetition

--- 2023/7.py
+++ 2023/7.py
@@ -5,33 +5,12 @@ from collections import Counter
 with open('7data') as f:
     data = [a.split(' ') for a in f.read().splitlines()]
 
-# def strength1(cards):
-#     c = sorted(Counter(cards).values(), reverse=True)
-#     match c:
-#         case [5]: return 7
-#         case [4, 1]: return 6
-#         case [3, 2]: return 5
-#         case [3, 1, 1]: return 4
-#         case [2, 2, 1]: return 3
-#         case [2, 1, 1, 1]: return 2
-#         case [1, 1, 1, 1, 1]: return 1
+part = 2
 
-# def mapper1(card):
-#     mapping = {'T': ':', 'J': ';', 'Q': '<', 'K': '=', 'A': '>'}
-#     card_map = mapping.get(card)
-#     return card_map if card_map else card
-
-# data = [(cards, strength1(cards), "".join(list(map(mapper1, cards))), int(bid)) for cards, bid in data]
-
-# ordered = sorted(data, key=lambda cards: (cards[1], cards[2]))
-# result = sum([a*b for a, b in zip([cards[3] for cards in ordered], range(1, len(ordered)+1))])
-
-# print(result)
-
-def strength2(cards):
+def strength(cards):
     counts = Counter(cards)
     c = sorted(counts.values(), reverse=True)
-    jacks = counts['J']
+    jacks = counts['J'] if part == 2 else 0
     match c:
         case [5]: return 7
         case [4, 1]: return 6 + {0: 0, 1: 1, 4: 1}[jacks]
@@ -41,12 +20,13 @@ def strength2(cards):
         case [2, 1, 1, 1]: return 2 + {0: 0, 1: 2, 2: 2}[jacks]
         case [1, 1, 1, 1, 1]: return 1 + {0: 0, 1: 1}[jacks]
 
-def mapper2(card):
-    mapping = {'T': ':', 'J': '0', 'Q': '<', 'K': '=', 'A': '>'} # updated J to 0
+def mapper(card):
+    j_val = ';' if part == 1 else '0'
+    mapping = {'T': ':', 'J': j_val, 'Q': '<', 'K': '=', 'A': '>'}
     card_map = mapping.get(card)
     return card_map if card_map else card
 
-data = [(cards, strength2(cards), "".join(list(map(mapper2, cards))), int(bid)) for cards, bid in data]
+data = [(cards, strength(cards), "".join(list(map(mapper, cards))), int(bid)) for cards, bid in data]
 
 ordered = sorted(data, key=lambda cards: (cards[1], cards[2]))
 result = sum([a*b for a, b in zip([cards[3] for cards in ordered], range(1, len(ordered)+1))])
