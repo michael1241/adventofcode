4d6e95f381594d4e4fd89c45d886e9c2d1f272a6
michael <michael@michael-desktop.Home>
2024-12-01 14:33:27 +0000

    day 01 rust

---
+++ 2024/src/days/day01.rs
@@ -0,0 +1,26 @@
+use crate::{Solution, SolutionPair};
+use std::fs::read_to_string;
+use counter::Counter;
+
+///////////////////////////////////////////////////////////////////////////////
+
+pub fn solve() -> SolutionPair {
+    let input = read_to_string("input/day01.txt").unwrap();
+    let pairs: Vec<_> = input.lines().map(parse_line).collect();
+    let (mut col1, mut col2): (Vec<_>, Vec<_>) = pairs.into_iter().unzip();
+    col1.sort();
+    col2.sort();
+    let diffs: Vec<_> = col1.iter().zip(col2.iter()).map(|(&x, &y)| (x - y).abs()).collect();
+    let sol1: i32 = diffs.iter().sum();
+
+    // first list has no duplicates
+    let counter2 = col2.iter().collect::<Counter<_>>();
+    let sol2: usize = col1.iter().map(|x| (*x as usize) * counter2[x]).collect::<Vec<_>>().into_iter().sum();
+
+    (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn parse_line(line: &str) -> (i32, i32) {
+    let (a, b) = line.split_once("   ").unwrap();
+    (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap())
+}
